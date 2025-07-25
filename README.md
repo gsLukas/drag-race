1d4158faa87c8f050f2ea5be3496f3ae9ddaa5c3
Grupo Sales Tecnologias <91630131+gsLukas@users.noreply.github.com>
2025-07-25 15:50:15 -0300

    subir todos arquivos iniciais do projeto

---
+++ README.md
@@ -0,0 +1,22 @@
+# qb-drag
+
+Este é um recurso para FiveM que implementa corridas de arrancada (drag race) para servidores baseados em QBCore.
+
+## Instalação
+
+1. Coloque os arquivos na pasta de recursos do seu servidor.
+2. Adicione `ensure qb-drag` ao seu `server.cfg`.
+
+## Funcionalidades
+- Sistema de corridas de arrancada
+- Interface NUI personalizada
+- Configuração flexível via `config.lua`
+
+## Créditos
+Desenvolvido por gsLukas.
+
+## Licença
+Consulte o arquivo LICENSE para detalhes.
+
+---
+Para dúvidas, sugestões ou contribuições, abra uma issue ou pull request neste repositório.
---
+++ fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'qb-race - Corrida de arrancada (drag race) + Sistema de XP e Customizações'
+author 'Grupo Sales, pikolin0 & gsLukao'
+version '2.7.1'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua',
+    'client/main.lua',
+    'client/mods.lua'
+}
+
+server_scripts {
+    'server.lua',
+    '@qb-core/server/main.lua',
+    'server/main.lua'
+}
+
+exports {
+    'AddXP_Treino',
+    'AddXP_Disputa',
+    'AddXP_Vitoria'
+}
+
+ui_page 'html/nui.html'
+
+files {
+    'html/nui.html',
+    'html/**/*.js',
+    'html/**/*.mp3',
+    'html/**/*.svg',
+    'html/img/*',
+    'html/**/*.css',
+    'html/sounds/*'
+}
